57fcd844f01887d13bd9ac0f01fae898b3dd13eb
realdog <cnodejsolddog@gmail.com>
2013-01-16 12:48:15 +0800

    change require path

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
-    var index = require('..controllers/index');
+    var index = require('../controllers/index');
     app.get('/index', index.getIndex);
     app.post('/index', index.postIndex);
 };
